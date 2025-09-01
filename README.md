cf04da30cc6e882eb2b3223f65cb5928175ec455
Patrick <patrick@tangentsolutions.co.za>
2025-09-01 09:38:35 +0200

    making sure the dummy page is showing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Chess club
+# Chess club administration app
 The local chess club wants to keep track of their members. The main thing that they want to
-keep track of, is the ranking of each member.
+keep track of is the ranking of each member.
 
 ## Running the project
 
--- src/main/java/za/co/tangentsolutions/chessclub/Application.java
+++ src/main/java/za/co/tangentsolutions/chessclub/Application.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class Application {
     public static void main(String[] args) {
+        System.out.println(args);
         SpringApplication.run(Application.class, args);
     }
 }
