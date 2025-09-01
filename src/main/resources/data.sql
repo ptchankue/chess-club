6de5680c68d364bfed40d1f50476427d2d3ab30c
Patrick <patrick@tangentsolutions.co.za>
2025-09-01 14:21:43 +0200

    adding some data on startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+    -- dummy data insertions
+    INSERT INTO member (id, name, surname, email, birthday, games_played, rank) VALUES (1, 'Carlos', 'Rodriguez', 'carlos@chessclub.co.za', '1997-02-20', 1, 1);
