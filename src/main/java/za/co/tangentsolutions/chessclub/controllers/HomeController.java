1883f90b7a5a786dece3e0dd6882fd6817b0649d
Patrick <patrick@tangentsolutions.co.za>
2025-09-01 08:55:31 +0200

    Adding required dependencies and a dummy controller

--- src/main/java/za/co/tangentsolutions/chessclub/controllers/HomeController.java
+++ src/main/java/za/co/tangentsolutions/chessclub/controllers/HomeController.java
@@ -1,4 +1,12 @@
 package za.co.tangentsolutions.chessclub.controllers;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HomeController {
+
+    @GetMapping(value = {"/", "/home", "index"})
+    public String homePage(Model model) {
+        return "Hello there!";
+    }
 }
